93fa9f93b15c442fd8d7fcc7406e38bb68ef03ca
Cross_Lee <mag_lee@126.com>
2013-09-22 17:52:07 +0800

    add folder and ini check

--- src/com/withiter/utils/InfoUtils.java
+++ src/com/withiter/utils/InfoUtils.java
@@ -24,8 +24,8 @@ public class InfoUtils {
 		System.out.println("start to check USB's status。。。");
 
 		// TODO update drive path here
-//		String rootPath = "I:\\";
-//		USBConfig.drivePath = "I:\\";
+		// String rootPath = "I:\\";
+		// USBConfig.drivePath = "I:\\";
 		String rootPath = USBConfig.drivePath;
 		if (rootPath == null) {
 			JOptionPane.showMessageDialog(null, "未检测到U盘，请在登陆前插入U盘。点击确定退出系统",
@@ -43,17 +43,22 @@ public class InfoUtils {
 		System.out.println(videonewFolder);
 
 		File videoFilesPath = new File(videonewFolder);
+		if (!videoFilesPath.exists()) {
+			videoFilesPath.mkdirs();
+		}
 		File[] videos = videoFilesPath.listFiles();
 		Video v = null;
 		for (File f : videos) {
 			System.out.println(f.getName());
-			if(USBConfig.EXTS.contains(StringUtils.getSuffixWithoutDot(f.getName()))){
+			if (USBConfig.EXTS.contains(StringUtils.getSuffixWithoutDot(f
+					.getName()))) {
 				String name = f.getName();
-				String ext = name.substring(name.lastIndexOf("."), name.length());
+				String ext = name.substring(name.lastIndexOf("."),
+						name.length());
 				String path = f.getAbsolutePath();
 				String size = String.valueOf((f.length() / 1024 / 1024));
 				String updateDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-				.format(new Date(f.lastModified()));
+						.format(new Date(f.lastModified()));
 				v = new Video(name, ext, path, size, updateDate);
 				VideoDao.instance().addVideo(v);
 			}
@@ -70,18 +75,19 @@ public class InfoUtils {
 		File temperatureIni = new File(temperatureiniFile);
 		if (!temperatureIni.exists()) {
 			System.out.println(temperatureiniFile + " file does not exist!");
-		} else {
-			List<String> list = FileReaderUtils.readFile(temperatureIni);
-			if (list != null) {
-				Temperature temp = null;
-				for (String s : list) {
-					if (s.startsWith("strdata")) {
-						int spliterIndex = s.indexOf("=");
-						String content = s.substring(spliterIndex + 1,
-								s.length());
-						temp = new Temperature(content);
-						TemperatureDao.instance().addTemperature(temp);
-					}
+			File f = new File("/files/temperature.ini");
+			FileReaderUtils.copy(f, temperatureIni);
+		}
+
+		List<String> list = FileReaderUtils.readFile(temperatureIni);
+		if (list != null) {
+			Temperature temp = null;
+			for (String s : list) {
+				if (s.startsWith("strdata")) {
+					int spliterIndex = s.indexOf("=");
+					String content = s.substring(spliterIndex + 1, s.length());
+					temp = new Temperature(content);
+					TemperatureDao.instance().addTemperature(temp);
 				}
 			}
 		}
@@ -98,19 +104,20 @@ public class InfoUtils {
 		File newsIni = new File(newsiniFile);
 		if (!newsIni.exists()) {
 			System.out.println(newsiniFile + " file does not exist!");
-		} else {
-			List<String> list = FileReaderUtils.readFile(newsIni);
-			if (list != null) {
-				News news = null;
-				for (String s : list) {
-					if (s.startsWith("news")) {
-						int spliterIndex = s.indexOf("=");
-						String index = s.substring(0, spliterIndex);
-						String content = s.substring(spliterIndex + 1,
-								s.length());
-						news = new News(index, content);
-						NewsDao.instance().addNews(news);
-					}
+			File f = new File("/files/news.ini");
+			FileReaderUtils.copy(f, newsIni);
+		}
+
+		List<String> list = FileReaderUtils.readFile(newsIni);
+		if (list != null) {
+			News news = null;
+			for (String s : list) {
+				if (s.startsWith("news")) {
+					int spliterIndex = s.indexOf("=");
+					String index = s.substring(0, spliterIndex);
+					String content = s.substring(spliterIndex + 1, s.length());
+					news = new News(index, content);
+					NewsDao.instance().addNews(news);
 				}
 			}
 		}
@@ -127,70 +134,69 @@ public class InfoUtils {
 		File logtext = new File(logfile);
 		if (!logtext.exists()) {
 			System.out.println(logtext + " file does not exist!");
-		} else {
-			List<String> list = FileReaderUtils.readFile(logtext);
-			System.out.println("logs's size is: " + list.size());
-			if (list != null) {
-				Log log = null;
-				for (int i = 0; i < list.size(); i += 3) {
+			File f = new File("/files/logfile.ini");
+			FileReaderUtils.copy(f, logtext);
+		}
+		List<String> list = FileReaderUtils.readFile(logtext);
+		System.out.println("logs's size is: " + list.size());
+		if (list != null) {
+			Log log = null;
+			for (int i = 0; i < list.size(); i += 3) {
 
-					System.out.println("line: " + i + "," + list.get(i));
-					System.out.println("line: " + (i + 1) + ","
-							+ list.get(i + 1));
-					System.out.println("line: " + (i + 2) + ","
-							+ list.get(i + 2));
+				System.out.println("line: " + i + "," + list.get(i));
+				System.out.println("line: " + (i + 1) + "," + list.get(i + 1));
+				System.out.println("line: " + (i + 2) + "," + list.get(i + 2));
 
-					String ip = list.get(i).split(":")[1];
-					String mac = list.get(i + 1).split(":")[1];
-					String date = "";
-					String operation = "";
-					String filename = "";
-					String result = "";
+				String ip = list.get(i).split(":")[1];
+				String mac = list.get(i + 1).split(":")[1];
+				String date = "";
+				String operation = "";
+				String filename = "";
+				String result = "";
 
-					if (list.get(i + 2).contains("更新")) {
-						String[] aStr = list.get(i + 2).split("更新");
-						date = aStr[0];
-						operation = "更新";
-						if (aStr[1].contains("成功")) {
-							filename = aStr[1].replace("成功", "");
-							result = "成功";
-						}
-						if (aStr[1].contains("失败")) {
-							filename = aStr[1].replace("失败", "");
-							result = "失败";
-						}
+				if (list.get(i + 2).contains("更新")) {
+					String[] aStr = list.get(i + 2).split("更新");
+					date = aStr[0];
+					operation = "更新";
+					if (aStr[1].contains("成功")) {
+						filename = aStr[1].replace("成功", "");
+						result = "成功";
 					}
-					if (list.get(i + 2).contains("添加")) {
-						String[] aStr = list.get(i + 2).split("添加");
-						date = aStr[0];
-						operation = "添加";
-						if (aStr[1].contains("成功")) {
-							filename = aStr[1].replace("成功", "");
-							result = "成功";
-						}
-						if (aStr[1].contains("失败")) {
-							filename = aStr[1].replace("失败", "");
-							result = "失败";
-						}
+					if (aStr[1].contains("失败")) {
+						filename = aStr[1].replace("失败", "");
+						result = "失败";
 					}
-					if (list.get(i + 2).contains("删除")) {
-						String[] aStr = list.get(i + 2).split("删除");
-						date = aStr[0];
-						operation = "删除";
-						if (aStr[1].contains("成功")) {
-							filename = aStr[1].replace("成功", "");
-							result = "成功";
-						}
-						if (aStr[1].contains("失败")) {
-							filename = aStr[1].replace("失败", "");
-							result = "失败";
-						}
-					}
-
-					log = new Log(ip, mac, date, operation, filename, result);
-					System.out.println(log.toString());
-					LogDao.instance().addLog(log);
 				}
+				if (list.get(i + 2).contains("添加")) {
+					String[] aStr = list.get(i + 2).split("添加");
+					date = aStr[0];
+					operation = "添加";
+					if (aStr[1].contains("成功")) {
+						filename = aStr[1].replace("成功", "");
+						result = "成功";
+					}
+					if (aStr[1].contains("失败")) {
+						filename = aStr[1].replace("失败", "");
+						result = "失败";
+					}
+				}
+				if (list.get(i + 2).contains("删除")) {
+					String[] aStr = list.get(i + 2).split("删除");
+					date = aStr[0];
+					operation = "删除";
+					if (aStr[1].contains("成功")) {
+						filename = aStr[1].replace("成功", "");
+						result = "成功";
+					}
+					if (aStr[1].contains("失败")) {
+						filename = aStr[1].replace("失败", "");
+						result = "失败";
+					}
+				}
+
+				log = new Log(ip, mac, date, operation, filename, result);
+				System.out.println(log.toString());
+				LogDao.instance().addLog(log);
 			}
 		}
 	}
