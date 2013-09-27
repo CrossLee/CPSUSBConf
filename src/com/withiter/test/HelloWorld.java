7032308620667b915f9e5ed69c9b44dc99be9a69
Cross_Lee <mag_lee@126.com>
2013-09-27 13:37:07 +0800

    jna test

--- src/com/withiter/test/HelloWorld.java
+++ src/com/withiter/test/HelloWorld.java
@@ -6,7 +6,6 @@ import com.sun.jna.Platform;
 
 /** Simple example of JNA interface mapping and usage. */
 public class HelloWorld {
-
     // This is the standard, stable way of mapping, which supports extensive
     // customization and mapping of Java to native types.
 	//BOOL  WriteProfileStr(const WCHAR* sSection,const WCHAR* sEntry,const WCHAR* sValue,const WCHAR* sIniPath)
